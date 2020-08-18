62b5466a416873bb261620080a8c579c6f11618c
Furkan Enes Apaydın <feapaydin@gmail.com>
2020-08-19 01:04:52 +0300

    added comments to sample sql

--- spec/db/mysql.sql
+++ spec/db/mysql.sql
@@ -1,3 +1,9 @@
+--------
+-- Migrations will create the database automatically. 
+-- You can use this SQL as a reference 
+-- or execute this SQL to create the tables if migrations somehow won't work.
+--------
+
 CREATE TABLE users(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	visible_id VARCHAR(50),
